a1ffb883da851c8ac6648eac57b523f018b25cfb
Tahsin Mostafiz <tahsin.mostafiz@ibm.com>
2020-03-10 22:03:59 -0400

    fixed location details

--- src/components/signup/AddressForm.js
+++ src/components/signup/AddressForm.js
@@ -1,42 +1,41 @@
 import React, { Component } from 'react'
 
 class AddressForm extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            latitude: null, 
-            longitude: null,
-        }
-        this.getCurrentLocation = this.getCurrentLocation.bind(this);
+  constructor(props) {
+    super(props)
+    this.state = {
+        latitude: null,
+        longitude: null,
     }
+    this.getCurrentLocation = this.getCurrentLocation.bind(this);
+  }
 
 
-    getCurrentLocation = async () => { // async is asynchronous function 
-        await navigator.geolocation.getCurrentPosition( //await says to wait for response
-            position => this.setState({ 
-                latitude: position.coords.latitude.toFixed(3),
-                longitude: position.coords.longitude.toFixed(3) // toFixed sets decimal value 
-            }, () => this.handleLocationChange()), 
-            err => console.log(err)
-        );
-    }
+  getCurrentLocation = async () => { // async is asynchronous function
+    await navigator.geolocation.getCurrentPosition( //await says to wait for response
+      position => this.setState({
+        latitude: Number(position.coords.latitude.toFixed(3)),
+        longitude: Number(position.coords.longitude.toFixed(3)) // toFixed sets decimal value
+      }, () => this.handleLocationChange()),
+      err => console.log(err)
+    );
+  }
 
-    handleLocationChange = () => {
-        this.props.onLocationChange({
-            latitude: this.state.latitude, 
-            longitude: this.state.longitude
-        })
-    }
-  
+  handleLocationChange = () => {
+    this.props.onLocationChange({
+      latitude: this.state.latitude,
+      longitude: this.state.longitude
+    })
+  }
 
-    render() {
-        return (
-            <div>
-                <button onClick= {this.getCurrentLocation}>Use my current location</button>
-                
-            </div>
-        )
-    }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.getCurrentLocation}>Use my current location</button>
+      </div>
+    )
+  }
 }
 
 export default AddressForm
--- src/components/signup/AddressMap.js
+++ src/components/signup/AddressMap.js
@@ -3,29 +3,27 @@ import AddressForm from './AddressForm'
 import GroceryMap from './GroceryMap'
 
 class AddressMap extends Component {
-    constructor() {
-        super()
-        this.state= {
-            location: null
-        }
-        this.handleLocationChange = this.handleLocationChange.bind(this);
-
+  constructor() {
+    super()
+    this.state= {
+      location: null
     }
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+  }
 
-    handleLocationChange = (location) => {
-        this.setState({location: location})
-    }
+  handleLocationChange = (currentLocation) => {
+    this.setState({location: currentLocation})
+  }
 
 
-    render() {
-        console.log(this.state.location)
-        return (
-            <div>
-                <AddressForm onLocationChange={this.handleLocationChange} />
-                <GroceryMap isMarkerShown location={this.state.location} />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <AddressForm onLocationChange={this.handleLocationChange} />
+        { this.state.location && <GroceryMap isMarkerShown location={this.state.location} /> }
+      </div>
+    )
+  }
 }
 
 export default AddressMap
