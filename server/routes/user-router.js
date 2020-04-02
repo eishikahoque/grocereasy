fb3b04c021cc1f39d53a0d1d78c86bd699066f7d
rbiag <54857812+rbiag@users.noreply.github.com>
2020-04-01 21:25:48 -0400

    Update user-router.js

--- server/routes/user-router.js
+++ server/routes/user-router.js
@@ -4,8 +4,8 @@ const UserCtrl = require('../controllers/user-ctrl')
 
 const router = express.Router()
 
-router.post('/user', UserCtrl.createUser)
-router.patch('/user/:id', UserCtrl.updateUser)
-router.get('/user/:id', UserCtrl.getUser)
+router.post('/user', UserCtrl.createUser) //creates a new user
+router.patch('/user/:id', UserCtrl.updateUser) //updates a user's information based on id
+router.get('/user/:id', UserCtrl.getUser) //gets user based on id
 
 module.exports = router
