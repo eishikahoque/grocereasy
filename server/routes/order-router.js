0814ac616364b72d7de823ffd315880a72cf7bb7
rbiag <54857812+rbiag@users.noreply.github.com>
2020-04-04 19:55:24 -0400

    update bug fixed & get order by user id added

--- server/routes/order-router.js
+++ server/routes/order-router.js
@@ -6,6 +6,7 @@ const router = express.Router()
 
 router.post('/user/:user_id/order', OrderCtrl.createOrder)
 router.get('/order/:id', OrderCtrl.getOrderById)
+router.get('/user/:user_id/orders', OrderCtrl.getOrderByUserId)
 router.patch('/order/:id', OrderCtrl.updateOrder)
 
 module.exports = router
