2e3985f22e10bbd4e70cbc650b9d25d738f3d81e
Rbia <rbia.gilani@gmail.com>
2020-04-06 14:36:21 -0400

    merged hira's part

--- server/index.js
+++ server/index.js
@@ -1,23 +1,34 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
+const http = require('http')
+const socket = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socket(server)
 
 const db = require('./db')
 const userRouter = require('./routes/user-router')
 const orderRouter = require('./routes/order-router')
 
-const app = express();
-
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+app.get('/', function(req, res){
+  res.send('working');
+});
 
-app.get('/', (req, res) => {
-    res.send('hello');
-})
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.use('/api', userRouter)
 app.use('/api', orderRouter)
+
+io.on('connection', (socket) => {
+  socket.emit('your id', socket.id)
+  socket.on('send message', (body) => {
+    io.emit('message', body)
+  })
+})
 
 app.listen(8000, () => {
     console.log('listening on port 8000');
